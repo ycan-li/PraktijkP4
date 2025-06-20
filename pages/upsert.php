624520bcd6990b217553cdf88e943066f8beaabf
ycan-li <yincan.li@edu-kw1c.nl>
2025-06-20 15:55:24 +0200

    chore: rename to upsert

--- pages/upsert.php
+++ pages/upsert.php
@@ -188,6 +188,6 @@ $categories = [
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
         crossorigin="anonymous"></script>
-<script type="module" src="../js/add.js"></script>
+<script type="module" src="../js/upsert.js"></script>
 </body>
 </html>
